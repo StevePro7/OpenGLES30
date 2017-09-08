539a055b4e978e4d173b8bb33e39c0dbd45e7b71
StevePro7 <steveprozune@hotmail.com>
2017-09-08 14:01:10 +0100

    Add common directory

---
+++ Common/Inc/esUtil_win.h
@@ -0,0 +1,15 @@
+#ifndef ESUTIL_WIN_H
+#define ESUTIL_WIN_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+GLboolean WinCreate( ESContext* esContext, const char* title );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif//ESUTIL_WIN_H
