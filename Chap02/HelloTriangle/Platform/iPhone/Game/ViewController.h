3d6c8734222e3e3e39a3e85d6bcf4ac3c768061f
StevePro7 <steveprozune@hotmail.com>
2017-09-08 14:35:57 +0100

    Add iPhone project

---
+++ Chap02/HelloTriangle/Platform/iPhone/Game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Game
+//
+//  Created by Steven Boland on 08/09/2017.
+//  Copyright © 2017 StevePro Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Chap02/HelloTriangle/Platform/iPhone/Game/ViewController.mm
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Game
+//
+//  Created by Steven Boland on 08/09/2017.
+//  Copyright © 2017 StevePro Studios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
