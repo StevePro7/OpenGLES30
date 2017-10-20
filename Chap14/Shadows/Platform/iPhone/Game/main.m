0bfd7bcb55670050cd0a08592b0b6f07853fe97d
StevePro7 <steveprozune@hotmail.com>
2017-10-20 13:30:40 +0100

    Update Chap14

---
+++ Chap14/Shadows/Platform/iPhone/Game/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Game
+//
+//  Created by Steven Boland on 08/09/2017.
+//  Copyright © 2017 StevePro Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
